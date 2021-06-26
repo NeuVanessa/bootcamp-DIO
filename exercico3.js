4da726576b0f630e3bd0bd57cea5a4d8ca0537cc
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-06-26 12:46:48 -0300

    nota do aluno mostrando nome e a nota final

---
+++ exercico3.js
@@ -0,0 +1,15 @@
+// Exercicio Nota 2
+
+var aluno = 'Vanessa';
+media = ((5 + 3 + 3 + 2) / 4);
+
+if (media >= 7 && media < 10) {
+    console.log(`${aluno} Foi aprovado na nota de`)
+}
+if (media >= 10) {
+    console.log(`${aluno} Mestre dos Magos miserável tu é genio sua nota: ${media}`)
+} if (media < 7) {
+    console.log(`${aluno} Está Reprovado Sua Nota foi:${media}`)
+
+}
+
