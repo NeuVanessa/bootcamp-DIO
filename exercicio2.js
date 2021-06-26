1c46f6ddf54cfd6c122d475fc4d78ba5c7e3eea6
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-06-26 12:05:26 -0300

    exercicio atravessar

---
+++ exercicio2.js
@@ -0,0 +1,13 @@
+//Iniciando a aula 2 algoritmo ovelha não coloquei a lógica dos outros animais, pois segue da mesma forma para todos.
+
+let ovelha = 'Nando';
+let barco = false;
+if (barco = barco) {
+    atravessar = true;
+    console.log(`A OVELHA ${ovelha} ENTROU NO BARCO `);
+    console.log('PODE ATRAVESSAR?' + atravessar)
+} else {
+    atravessar = false;
+    console.log(` A O VELHA ${ovelha} SAIU DO BARCO`)
+    console.log('PODE ATRAVESSAR? ' + atravessar)
+}
