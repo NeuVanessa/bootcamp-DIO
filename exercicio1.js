03066bcdc620e69b858fafdb0b12bfee5710d1cc
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-06-26 11:41:16 -0300

    iniciando o bootcamp santander

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+//EXERCICIO FINAL DA AULA DE ALGORITIMO NA DIO
+
+let n1 = 5;
+let n2 = 7;
+let n3 = 10;
+let n4 = 5;
+
+nota = ((n1 + n2 + n3 + n4) / 4);
+
+console.log('A NOTA FINAL DESSE ALUNO É: ' + nota);
